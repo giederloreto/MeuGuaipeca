1af200a0178ba8c1c195910633c4b0b4fbf92669
Gieder Oliveira de Loreto <gieder.gol@gmail.com>
2022-04-22 15:55:10 -0300

    Custom AppBar

---
+++ lib/presentation/components/guaipeca_appbar.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class GuaipecaAppBar extends StatefulWidget implements PreferredSizeWidget {
+  const GuaipecaAppBar({Key? key}) : super(key: key);
+
+  @override
+  State<GuaipecaAppBar> createState() => _GuaipecaAppBarState();
+
+  @override
+  // TODO: implement preferredSize
+  Size get preferredSize {
+    var deviceWidth = const Size.fromHeight(100);
+
+    return deviceWidth;
+  }
+}
+
+class _GuaipecaAppBarState extends State<GuaipecaAppBar> {
+  @override
+  Widget build(BuildContext context) {
+    return PreferredSize(
+      preferredSize: const Size.fromHeight(100),
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          // ignore: prefer_const_literals_to_create_immutables
+          actions: [
+            const CircleAvatar(
+              child: Icon(Icons.person),
+              maxRadius: 30,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/presentation/home/home_view.dart
+++ lib/presentation/home/home_view.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:meu_guaipeca/presentation/components/guaipeca_appbar.dart';
 import 'package:meu_guaipeca/presentation/components/guaipeca_bottom_bar.dart';
-import 'package:meu_guaipeca/presentation/components/guaipeca_icon.dart';
 
 class HomeView extends StatefulWidget {
   const HomeView({Key? key}) : super(key: key);
@@ -16,19 +16,24 @@ class _HomeViewState extends State<HomeView> {
   Widget build(BuildContext context) {
     return Scaffold(
       extendBody: true,
-      body: ListView.builder(
-        itemCount: feed.length,
-        itemBuilder: (context, index) {
-          return const Card(
-            child: ListTile(
-              title: Text(
-                'Titulo',
+      extendBodyBehindAppBar: true,
+      appBar: const GuaipecaAppBar(),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+          itemCount: feed.length,
+          itemBuilder: (context, index) {
+            return const Card(
+              child: ListTile(
+                title: Text(
+                  'Titulo',
+                ),
+                subtitle: Text('Subtitulo'),
+                leading: Icon(Icons.pets),
               ),
-              subtitle: Text('Subtitulo'),
-              leading: Icon(Icons.pets),
-            ),
-          );
-        },
+            );
+          },
+        ),
       ),
       bottomNavigationBar: const GuaipecaBottomBar(),
     );
