a7a178c59eafb4eb27a8fd4309dc9dce18ba3188
giederloreto <gieder.gol@gmail.com>
2022-10-27 21:50:02 -0300

    refact: improve login view

--- lib/presentation/login/view/login.dart
+++ lib/presentation/login/view/login.dart
@@ -21,212 +21,187 @@ class _LoginViewState extends State<LoginView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Stack(
-                alignment: Alignment.bottomCenter,
-                children: [
-                  SizedBox(
-                    child: Lottie.network(
-                        "https://assets5.lottiefiles.com/private_files/lf30_fz6lkjf0.json"),
-                  ),
-                  const Padding(
-                    padding: EdgeInsets.only(bottom: 16.0),
-                    child: Text(
-                      'Meu Guaipeca',
-                      style: TextStyle(
-                          fontFamily: 'Quicksand',
-                          fontSize: 34,
-                          fontWeight: FontWeight.w900,
-                          color: Colors.white),
-                    ),
-                  ),
-                ],
-              ),
-
-              /* Image.asset(
-                'assets/images/guaipeca_logo.png',
-                height: MediaQuery.of(context).size.height * 0.30,
-                width: MediaQuery.of(context).size.width * 0.15,
-              ), */
-              SizedBox(
-                height: MediaQuery.of(context).size.height * 0.01,
-              ),
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: TextFormField(
-                  controller: _loginViewModel.emailController,
-                  keyboardType: TextInputType.text,
-                  decoration: InputDecoration(
-                    contentPadding: const EdgeInsets.all(8),
-                    filled: true,
-                    fillColor: Colors.white70,
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(15),
-                    ),
-                    prefixIcon: const Icon(
-                      Icons.mail,
-                      color: Colors.black,
-                    ),
-                    label: Text(
-                      Strings.strings.strEmail,
-                      style: const TextStyle(
-                          fontFamily: 'Quicksand',
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold),
-                    ),
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: TextFormField(
-                  controller: _loginViewModel.passwordController,
-                  keyboardType: TextInputType.text,
-                  obscureText: !_isVisible,
-                  decoration: InputDecoration(
-                    contentPadding: const EdgeInsets.all(8),
-                    filled: true,
-                    fillColor: Colors.white70,
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(15),
-                    ),
-                    suffixIcon: IconButton(
-                        color: Colors.black,
-                        onPressed: () {
-                          setState(() {
-                            _isVisible = !_isVisible;
-                          });
-                        },
-                        icon: _isVisible
-                            ? const Icon(
-                                Icons.visibility,
-                                color: Colors.black,
-                              )
-                            : const Icon(
-                                Icons.visibility_off,
-                                color: Colors.black,
-                              )),
-                    label: Text(
-                      Strings.strings.senha,
-                      style: TextStyle(
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Stack(
+              alignment: Alignment.bottomCenter,
+              children: [
+                Container(
+                    child: Lottie.asset('assets/images/dog-car-ride.json',
+                        repeat: true)),
+                const Padding(
+                  padding: EdgeInsets.only(bottom: 16.0),
+                  child: Text(
+                    'Meu Guaipeca',
+                    style: TextStyle(
                         fontFamily: 'Quicksand',
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
+                        fontSize: 34,
+                        fontWeight: FontWeight.w900,
+                        color: Colors.white),
                   ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(
-                    top: 8, left: 16.0, right: 16.0, bottom: 8),
-                child: SizedBox(
-                  height: 50,
-                  child: Card(
-                    elevation: 8,
-                    clipBehavior: Clip.hardEdge,
-                    child: InkWell(
-                      onTap: () async {
-                        try {
-                          await _loginViewModel.login().then((value) =>
-                              Navigator.pushNamed(context, RoutesNames.HOME));
-
-                          log("Logou VIEW");
-                        } on Exception catch (e) {
-                          log("Não LOGOU porque? $e");
-                          Navigator.pushNamed(
-                              context, RoutesNames.FORGETPASSWORD);
-                        }
-                      },
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          const SizedBox(height: 16),
-                          Text(
-                            Strings.strings.login,
-                            style: const TextStyle(
-                                fontFamily: 'Quicksand',
-                                fontSize: 18,
-                                fontWeight: FontWeight.bold),
-                          ),
-                          const SizedBox(height: 16),
-                        ],
-                      ),
-                    ),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 24, left: 24.0, right: 24),
+              child: TextFormField(
+                controller: _loginViewModel.emailController,
+                keyboardType: TextInputType.text,
+                decoration: InputDecoration(
+                  contentPadding: const EdgeInsets.all(8),
+                  filled: true,
+                  fillColor: Colors.white70,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15),
                   ),
-                ),
-              ),
-              const Divider(
-                thickness: 2,
-              ),
-              Padding(
-                padding: const EdgeInsets.only(
-                    top: 8, left: 16.0, right: 16.0, bottom: 8),
-                child: ImageTextButton(
-                  onPressed: () {},
-                  imageHeight: 100,
-                  text: Text(
-                    Strings.strings.loginWithGoogle,
+                  prefixIcon: const Icon(
+                    Icons.mail,
+                    color: Colors.black,
+                  ),
+                  label: Text(
+                    Strings.strings.strEmail,
                     style: const TextStyle(
                         fontFamily: 'Quicksand',
                         fontSize: 18,
                         fontWeight: FontWeight.bold),
                   ),
-                  path: 'assets/images/google.png',
                 ),
               ),
-              SizedBox(
-                height: MediaQuery.of(context).size.height * 0.005,
-              ),
-              Column(
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        Strings.strings.dontHaveAccount,
-                        style: const TextStyle(
-                            fontFamily: 'Quicksand',
-                            fontSize: 12,
-                            fontWeight: FontWeight.bold),
-                      ),
-                      TextButton(
-                        onPressed: () {
-                          Navigator.pushNamed(
-                              context, RoutesNames.REGISTERCHOOSE);
-                        },
-                        child: Text(Strings.strings.subscribe),
-                      ),
-                    ],
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 24, left: 24.0, right: 24),
+              child: TextFormField(
+                controller: _loginViewModel.passwordController,
+                keyboardType: TextInputType.text,
+                obscureText: !_isVisible,
+                decoration: InputDecoration(
+                  contentPadding: const EdgeInsets.all(8),
+                  filled: true,
+                  fillColor: Colors.white70,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15),
                   ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        Strings.strings.forgetPassword,
-                        style: const TextStyle(
-                            fontFamily: 'Quicksand',
-                            fontSize: 12,
-                            fontWeight: FontWeight.bold),
-                      ),
-                      TextButton(
-                        onPressed: () {
-                          Navigator.pushNamed(
-                              context, RoutesNames.FORGETPASSWORD);
-                        },
-                        child: Text(Strings.strings.resetPassword),
-                      ),
-                    ],
+                  suffixIcon: IconButton(
+                      color: Colors.black,
+                      onPressed: () {
+                        setState(() {
+                          _isVisible = !_isVisible;
+                        });
+                      },
+                      icon: _isVisible
+                          ? const Icon(
+                              Icons.visibility,
+                              color: Colors.black,
+                            )
+                          : const Icon(
+                              Icons.visibility_off,
+                              color: Colors.black,
+                            )),
+                  label: Text(
+                    Strings.strings.senha,
+                    style: TextStyle(
+                      fontFamily: 'Quicksand',
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 24, left: 24.0, right: 24),
+              child: SizedBox(
+                height: 50,
+                child: Card(
+                  elevation: 8,
+                  clipBehavior: Clip.hardEdge,
+                  child: InkWell(
+                    onTap: () async {
+                      try {
+                        await _loginViewModel.login().then((value) =>
+                            Navigator.pushNamed(context, RoutesNames.HOME));
+
+                        log("Logou VIEW");
+                      } on Exception catch (e) {
+                        log("Não LOGOU porque? $e");
+                        Navigator.pushNamed(
+                            context, RoutesNames.FORGETPASSWORD);
+                      }
+                    },
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const SizedBox(height: 16),
+                        Text(
+                          Strings.strings.login,
+                          style: const TextStyle(
+                              fontFamily: 'Quicksand',
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 8, left: 24.0, right: 24),
+              child: ImageTextButton(
+                onPressed: () {},
+                text: Text(
+                  Strings.strings.loginWithGoogle,
+                  style: const TextStyle(
+                      fontFamily: 'Quicksand',
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold),
+                ),
+                path: 'assets/images/google.png',
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 24.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    Strings.strings.dontHaveAccount,
+                    style: const TextStyle(
+                        fontFamily: 'Quicksand',
+                        fontSize: 12,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, RoutesNames.REGISTERCHOOSE);
+                    },
+                    child: Text(Strings.strings.subscribe),
                   ),
                 ],
-              )
-            ],
-          ),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  Strings.strings.forgetPassword,
+                  style: const TextStyle(
+                      fontFamily: 'Quicksand',
+                      fontSize: 12,
+                      fontWeight: FontWeight.bold),
+                ),
+                TextButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, RoutesNames.FORGETPASSWORD);
+                  },
+                  child: Text(Strings.strings.resetPassword),
+                ),
+              ],
+            )
+          ],
         ),
       ),
     );
