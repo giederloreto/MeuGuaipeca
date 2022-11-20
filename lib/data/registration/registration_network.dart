91c491bc8f29205af1e38b842e15e47078fc4af7
giederloreto <gieder.gol@gmail.com>
2022-11-20 16:46:53 -0300

    refactore: delete imports without use and add path on the gitignore

--- lib/data/registration/registration_network.dart
+++ lib/data/registration/registration_network.dart
@@ -1,8 +1,6 @@
 import 'dart:developer';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/material.dart';
 import 'package:meu_guaipeca/data/registration/registration_network_interface.dart';
-import 'package:meu_guaipeca/settings/routes/routes_names.dart';
 
 class RegistrationNetwork implements IRegistrationNetwork {
   late final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
