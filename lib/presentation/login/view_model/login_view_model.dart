ef3e150b643a0f01530705948112b56b557b600a
giederloreto <gieder.gol@gmail.com>
2022-12-12 19:59:10 -0300

    identação

--- lib/presentation/login/view_model/login_view_model.dart
+++ lib/presentation/login/view_model/login_view_model.dart
@@ -17,26 +17,26 @@ class LoginViewModel extends ChangeNotifier {
       await _loginBusiness
           .login(emailController.text, passwordController.text)
           .then((value) => Navigator.pushNamed(context, RoutesNames.HOME));
-
     } catch (e) {
       log(e.toString());
     }
   }
 
-   Future<UserCredential> signInWithGoogle() async {
-  // Trigger the authentication flow
-  final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+  Future<UserCredential> signInWithGoogle() async {
+    // Trigger the authentication flow
+    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+    
+    // Obtain the auth details from the request
+    final GoogleSignInAuthentication? googleAuth =
+        await googleUser?.authentication;
 
-  // Obtain the auth details from the request
-  final GoogleSignInAuthentication? googleAuth = await googleUser?.authentication;
+    // Create a new credential
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth?.accessToken,
+      idToken: googleAuth?.idToken,
+    );
 
-  // Create a new credential
-  final credential = GoogleAuthProvider.credential(
-    accessToken: googleAuth?.accessToken,
-    idToken: googleAuth?.idToken,
-  );
-
-  // Once signed in, return the UserCredential
-  return await FirebaseAuth.instance.signInWithCredential(credential);
-}
+    // Once signed in, return the UserCredential
+    return await FirebaseAuth.instance.signInWithCredential(credential);
+  }
 }
