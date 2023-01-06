6d1cf044b5c0c0f3d01095c738627bad3dceb56e
giederloreto <gieder.gol@gmail.com>
2023-01-06 14:05:03 -0300

    feat: add property componente formfield

--- lib/presentation/components/guaipeca_text_form_field.dart
+++ lib/presentation/components/guaipeca_text_form_field.dart
@@ -3,8 +3,17 @@ import 'package:flutter/material.dart';
 class GuaipecaTextFormField extends StatefulWidget {
   final String label;
   final TextEditingController? controller;
+  final int? minLines;
+  final int? maxLines;
+  final TextInputType? keyboardType;
 
-  const GuaipecaTextFormField({required this.label, this.controller, Key? key})
+  const GuaipecaTextFormField(
+      {required this.label,
+      this.controller,
+      this.minLines,
+      this.maxLines,
+      this.keyboardType,
+      Key? key})
       : super(key: key);
 
   @override
@@ -15,7 +24,10 @@ class _GuaipecaTextFormFieldState extends State<GuaipecaTextFormField> {
   @override
   Widget build(BuildContext context) {
     return TextFormField(
+      minLines: widget.minLines,
+      maxLines: widget.maxLines,
       controller: widget.controller,
+      keyboardType: widget.keyboardType,
       decoration: InputDecoration(
         contentPadding: const EdgeInsets.all(8),
         filled: true,
